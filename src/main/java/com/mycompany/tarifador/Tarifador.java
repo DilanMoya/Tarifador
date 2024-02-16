dc712af923187bd3ec58a4cb744d67d8702aedd5
DilanMoya <damoya3@espe.edu.ec>
2024-02-16 14:43:17 -0500

    fisrt

---
+++ src/main/java/com/mycompany/tarifador/Tarifador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarifador;
+
+/**
+ *
+ * @author xpro3
+ */
+public class Tarifador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
